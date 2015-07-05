5f1402d6ff91aa4312a1cded89c2538518bf2722
Kari <zoecu3@gmail.com>
2015-07-05 23:45:33 +0800

    Codeforces_4A
    Simple;
    all even numbers meet requirements except number 2

---
+++ _4A_Watermelon/_4A_Watermelon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class _4A_Watermelon {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int w = s.nextInt();
+		if(w%2==1||w==2){
+			System.out.println("NO");
+		}else{
+			System.out.println("YES");
+		}
+
+	}
+
+}
